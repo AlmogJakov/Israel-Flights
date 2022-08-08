a99d9e2cfe9d499371e0b04d492e227324bd066e
AlmogJakov <almog1006@gmail.com>
2022-08-08 18:48:11 +0300

    fixed extended data pulling by axios (there are still bugs)

--- DataEntry/models/flights.js
+++ DataEntry/models/flights.js
@@ -6,26 +6,25 @@ router = express.Router();
 
 var data = {};
 
-async function getMoreFlightInfo(flightID) {
-  return await axios
-    .get(
-      `https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${flightID}`
-    )
-    .then(function (response) {
-      // handle success
-      data = response.data;
-      //var keys = Object.keys(data);
-      //const json = JSON.parse(JSON.stringify(data));
-      //console.log(json);
-      //console.log(flightID);
-      //console.log(json["airport"]["origin"]["position"]["longitude"]);
-      return data;
-    });
-}
+// // Make a request for a user with a given ID
+// axios.get('https://data-cloud.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds=41.449%2C21.623%2C16.457%2C53.063&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1')
+//   .then(function (response) {
+//     // handle success
+//     data = response.data
+//     //console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
 
-async function iterateFlights(database) {
+function iterateFlights(database) {
   var result = {};
   var keys = Object.keys(database);
+  //var json = JSON.parse(data)
   const json = JSON.parse(JSON.stringify(database));
   for (var i = 2; i < keys.length - 1; i++) {
     var filghtID = keys[i];
@@ -41,41 +40,7 @@ async function iterateFlights(database) {
       var time = json[filghtID][10];
       var src = json[filghtID][11];
       var dst = json[filghtID][12];
-      // get extended information
-      //console.log(getMoreFlightInfo(filghtID));
 
-      var moreinfo;
-      await getMoreFlightInfo(filghtID).then((extendedFlightInfo) => {
-        moreinfo = extendedFlightInfo;
-        //console.log(extendedFlightInfo);
-        // var src_longitude =
-        //   moreinfo["airport"]["origin"]["position"]["longitude"];
-        // var src_latitude =
-        //   moreinfo["airport"]["origin"]["position"]["latitude"];
-        // var dst_longitude =
-        //   moreinfo["airport"]["destination"]["position"]["longitude"];
-        // var dst_latitude =
-        //   moreinfo["airport"]["destination"]["position"]["latitude"];
-        // //var src_lon =
-        // var src_weather = weather.getWeather(src_longitude, src_latitude);
-        // var dst_weather = weather.getWeather(dst_longitude, dst_latitude);
-        //return data;
-      });
-
-      // var mydata = getMoreFlightInfo(filghtID);
-      //console.log(mydata);
-
-      // var moreinfo = JSON.parse(JSON.stringify(getMoreFlightInfo(filghtID)));
-      var src_longitude =
-        moreinfo["airport"]["origin"]["position"]["longitude"];
-      var src_latitude = moreinfo["airport"]["origin"]["position"]["latitude"];
-      var dst_longitude =
-        moreinfo["airport"]["destination"]["position"]["longitude"];
-      var dst_latitude =
-        moreinfo["airport"]["destination"]["position"]["latitude"];
-      // //var src_lon =
-      // var src_weather = weather.getWeather(src_longitude, src_latitude);
-      // var dst_weather = weather.getWeather(dst_longitude, dst_latitude);
       var data = {
         id: filghtID,
         on_ground: onGround,
@@ -85,30 +50,105 @@ async function iterateFlights(database) {
         time: time,
         source: src,
         destination: dst,
-        src_longitude: src_longitude,
-        src_latitude: src_latitude,
-        dst_longitude: dst_longitude,
-        dst_latitude: dst_latitude,
-        //src_weather: src_weather,
-        //dst_weather: dst_weather,
       };
       result[filghtID].push(data);
+      //console.log(JSON.stringify(result));
+
+      //console.log(onGround);
+      //alert(i)
+      //break;
     }
   }
   return result;
 }
 
-const flightsDetails = async (req, res) => {
+function useNull() {
+  return null;
+}
+
+// https://stackoverflow.com/questions/27101240/typeerror-converting-circular-structure-to-json-in-nodejs
+const replacerFunc = () => {
+  const visited = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (visited.has(value)) {
+        return;
+      }
+      visited.add(value);
+    }
+    return value;
+  };
+};
+
+const flightsDetails = (req, res) => {
   axios
     .get(
       "https://data-cloud.flightradar24.com/zones/fcgi/feed.js?faa=1&bounds=41.449%2C21.623%2C16.457%2C53.063&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1"
     )
-    .then(function (response) {
+    .then(async function (response) {
       // handle success
       data = response.data;
-      data = iterateFlights(data);
-      return res.status(200).json(data);
-      //console.log(response);
+
+      //data = iterateFlights(data);
+
+      // get only TLV keys
+      const json = JSON.parse(JSON.stringify(data));
+      var keys = Object.keys(data);
+      var TLVkeys = [];
+      for (var i = 2; i < keys.length - 1; i++) {
+        var filghtID = keys[i];
+        var onGround = json[filghtID][14];
+        if (
+          (onGround == "0" && json[filghtID][12] == "TLV") ||
+          json[filghtID][11] == "TLV"
+        ) {
+          TLVkeys.push(filghtID);
+        }
+      }
+      //console.log(TLVkeys);
+
+      let linksArr = [];
+      for (const key of TLVkeys) {
+        linksArr.push(
+          `https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${key}`
+        );
+      }
+      //console.log(linksArr.length);
+      var dodo = [];
+      await axios
+        .all(linksArr.map((l) => axios.get(l).catch(useNull)))
+        .then(
+          //(data) => console.log(data)
+          // function (response) {
+          //   (response) => console.log(response);
+          //   //return res.status(200).json(data[0]);
+          // }
+          //(data) => console.log(data[0])
+
+          axios.spread(async function (...info) {
+            // all requests are now complete
+            //console.log(info.length);
+            //console.log(info);
+            //return info;
+            //return res.status(200).json(info.data);
+            dodo.push(info);
+            //dodo = info.data;
+            //dodo = structuredClone(info.data);
+            //res.status(200).json(info.data);
+            //console.log(keys);
+          })
+        )
+        .catch((err) => {
+          //console.log("FAIL", err);
+          console.log("FAIL");
+        });
+      //console.log(dodo[0][0]["data"]);
+      //console.log(JSON.stringify(dodo[0], replacerFunc())[0]);
+      return res.status(200).json(dodo[0][0]["data"]);
+      // console.log(dodo);
+      //return res.send(JSON.parse(dodo));
+      //console.log(dodo);
+      //return dodo;
     })
     .catch(function (error) {
       // handle error
@@ -119,25 +159,6 @@ const flightsDetails = async (req, res) => {
     });
 };
 
-const flight = {
-  // get Weather by longitude (lon) and latitude (lat)
-  getMoreFlightInfo: function (flightID) {
-    axios
-      .get(
-        `https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${flightID}`
-      )
-      .then(function (response) {
-        // handle success
-        data = response.data;
-        var keys = Object.keys(data);
-        const json = JSON.parse(JSON.stringify(data));
-        //console.log(json);
-        console.log(json["airport"]["origin"]["name"]);
-        return json;
-      });
-  },
-};
-
 // router.get('/', function(req, res, next) {
 //   res.render('index', {title: 'Express'});
 // });
@@ -145,3 +166,38 @@ const flight = {
 module.exports = {
   flightsDetails,
 };
+
+// const accoutDetails = (req,res) => {
+//   let data = {test: "Test"}
+//   return res.status(200).json(data)
+// }
+
+// module.exports = {
+//   accoutDetails
+// }
+
+// ------------------------
+
+// const axiosOptions = {
+//   headers: {
+//     "X-Riot-Token": process.env.RIOTKEY,
+//   },
+// };
+
+// const getSummonerByName = async (region,summonerName) => {
+//     const res = await axios.get(config.summonerByNameUrl(region, summonerName), axiosOptions);
+//     return res.data;
+// }
+
+// const summonerParser = async (req,res) => {
+//     if(!req.query.summonerName || !req.query.region){
+//         return res.status(403).json({error: "Missing summoner name or region."});
+//     }
+//     const summonerData = await getSummonerByName(req.query.region, req.query.summonerName);
+//     return res.status(200).json(summonerData);
+
+// }
+
+// module.exports = {
+//     summonerParser
+// }
--- DataEntry/routes/controller.js
+++ DataEntry/routes/controller.js
@@ -2,8 +2,6 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 var mysql = require("../models/mysql");
-var weather = require("../models/weather");
-var flights = require("../models/flights");
 
 // auto format code: https://blog.yogeshchavan.dev/automatically-format-code-on-file-save-in-visual-studio-code-using-prettier
 
@@ -16,24 +14,7 @@ var flights = require("../models/flights");
 // })
 // module.exports = router;
 
-async function getMoreFlightInfo(flightID) {
-  return await axios
-    .get(
-      `https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${flightID}`
-    )
-    .then(function (response) {
-      // handle success
-      data = response.data;
-      //var keys = Object.keys(data);
-      //const json = JSON.parse(JSON.stringify(data));
-      //console.log(json);
-      //console.log(flightID);
-      //console.log(json["airport"]["origin"]["position"]["longitude"]);
-      return data;
-    });
-}
-
-async function iterateFlights(database) {
+function iterateFlights(database) {
   var result = {};
   var keys = Object.keys(database);
   const json = JSON.parse(JSON.stringify(database));
@@ -51,43 +32,7 @@ async function iterateFlights(database) {
       var time = json[filghtID][10];
       var src = json[filghtID][11];
       var dst = json[filghtID][12];
-      // get extended information
-      //console.log(getMoreFlightInfo(filghtID));
 
-      var moreinfo;
-      await getMoreFlightInfo(filghtID).then((extendedFlightInfo) => {
-        moreinfo = extendedFlightInfo;
-        //console.log(extendedFlightInfo);
-        // var src_longitude =
-        //   moreinfo["airport"]["origin"]["position"]["longitude"];
-        // var src_latitude =
-        //   moreinfo["airport"]["origin"]["position"]["latitude"];
-        // var dst_longitude =
-        //   moreinfo["airport"]["destination"]["position"]["longitude"];
-        // var dst_latitude =
-        //   moreinfo["airport"]["destination"]["position"]["latitude"];
-        // //var src_lon =
-        // var src_weather = weather.getWeather(src_longitude, src_latitude);
-        // var dst_weather = weather.getWeather(dst_longitude, dst_latitude);
-        //return data;
-      });
-
-      // var mydata = getMoreFlightInfo(filghtID);
-      //console.log(mydata);
-
-      // var moreinfo = JSON.parse(JSON.stringify(getMoreFlightInfo(filghtID)));
-
-      // var src_longitude =
-      //   moreinfo["airport"]["origin"]["position"]["longitude"];
-      // var src_latitude = moreinfo["airport"]["origin"]["position"]["latitude"];
-      // var dst_longitude =
-      //   moreinfo["airport"]["destination"]["position"]["longitude"];
-      // var dst_latitude =
-      //   moreinfo["airport"]["destination"]["position"]["latitude"];
-
-      // //var src_lon =
-      // var src_weather = weather.getWeather(src_longitude, src_latitude);
-      // var dst_weather = weather.getWeather(dst_longitude, dst_latitude);
       var data = {
         id: filghtID,
         on_ground: onGround,
@@ -97,27 +42,12 @@ async function iterateFlights(database) {
         time: time,
         source: src,
         destination: dst,
-        //src_longitude: src_longitude,
-        //src_latitude: src_latitude,
-        //dst_longitude: dst_longitude,
-        //dst_latitude: dst_latitude,
-        //src_weather: src_weather,
-        //dst_weather: dst_weather,
       };
       result[filghtID].push(data);
     }
   }
-  console.log("ddd");
-  //kafka.publish(JSON.stringify(result));
   return result;
 }
-
-// multiple axios requests: https://www.storyblok.com/tp/how-to-send-multiple-requests-using-axios
-
-// heb dates: https://www.hebcal.com/home/219/hebrew-date-converter-rest-api
-// weather: https://openweathermap.org/current
-// https://api.openweathermap.org/data/2.5/weather?q=Tel-Aviv&appid=bf97b4fc3170f7d6ebdd76d544691edf
-// https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
 
 // ----------------- Produce flights -----------------
 const kafka = require("../models/produceKafka");
@@ -136,7 +66,6 @@ const produce = async () => {
           // handle success
           mysql.access_writing("flightradar24");
           data = response.data;
-          console.log(iterateFlights(data));
           //kafka.publish(JSON.stringify(iterateFlights(data)));
         })
         .catch(function (error) {
@@ -157,6 +86,6 @@ produce();
 
 // ----------------- Client -----------------
 // Return current flight details to client
-//const flights = require("../models/flights");
+const flights = require("../models/flights");
 router.get("/", flights.flightsDetails);
 module.exports = router;
