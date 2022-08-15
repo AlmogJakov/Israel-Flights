d19c627523aa30a32ab32baeaa7ca39d383d4bae
AlmogJakov <almog1006@gmail.com>
2022-08-15 18:21:16 +0300

    rearranged DataEntry controller (removed logger func)

--- DataEntry/routes/controller.js
+++ DataEntry/routes/controller.js
@@ -1,37 +1,11 @@
 const express = require("express");
 const router = express.Router();
-var API = require("../models/Flights");
-
-// ------------------------------------ Log to file -------------------------------------
-// source: https://stackoverflow.com/questions/41232578/node-js-console-log-in-txt-file
-var log4js = require("log4js");
-log4js.configure({
-  appenders: {
-    fileLog: { type: "file", filename: "logs/mylog.log" },
-    console: { type: "console" },
-  },
-  categories: {
-    file: { appenders: ["fileLog"], level: "error" },
-    another: { appenders: ["console"], level: "trace" },
-    default: { appenders: ["console", "fileLog"], level: "trace" },
-  },
-});
-var logger = log4js.getLogger("fileLog");
-//logger.debug("Cheese is not a food.");
-// ---------------------------------- END Log to file -----------------------------------
-
-// auto format code: https://blog.yogeshchavan.dev/automatically-format-code-on-file-save-in-visual-studio-code-using-prettier
-
-// ----------------- Produce flights -----------------
-const kafka = require("../models/produceKafka");
-const produce = async () => {
-  produce_func = async () => {};
-};
-API.Run();
+var API = require("../models/API");
 
 // ----------------- Client -----------------
-// Return current flight details to client via api
+// Return current flight details to the client via api
 router.get("/", (req, res) => {
   res.send(API.getOutput());
 });
+
 module.exports = router;
--- DataEntry/serverA.js
+++ DataEntry/serverA.js
@@ -4,27 +4,24 @@ var server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   allowEIO3: true, // false by default
 });
-const kafka = require("./models/produceKafka");
-const controllerRouter = require("./routes/controller"); //controller
+const controllerRouter = require("./routes/controller");
+var API = require("./models/API");
 
 const bodyParser = require("body-parser");
 const path = require("path");
 
 //----------------- Middleware -----------------
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-//app.use(express.static("./views/myview"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.use(express.static("./views/static"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-//----------------Front side ------------------
+//---------------- Back side ------------------
+API.Run();
+
+//---------------- Front side ------------------
 app.use("/", controllerRouter);
-
-// --- Socket.io - Produce call details to kafka ----------------
-//const summoner = require('./models/flights');
-io.on("connection", (socket) => {
-  console.log("new user connected");
-});
 
 const Port = process.env.PORT | 3000;
 //http://localhost:3000
