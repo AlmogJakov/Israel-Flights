5b314d714881cae5de3440362b66256950138c5c
AlmogJakov <almog1006@gmail.com>
2022-08-17 07:56:19 +0300

    Changed to Cloud Redis (instead of Local) [tested on heroku]

--- Dashboard/models/connectRedis.js
+++ Dashboard/models/connectRedis.js
@@ -1,12 +1,21 @@
 const redis = require("ioredis");
 
+// https://app.redislabs.com/#/databases/11176232/subscription/1827195/view-bdb/metric
 const conn = {
-  port: 6379,
-  host: "127.0.0.1",
+  port: 12938,
+  host: "redis-12938.c59.eu-west-1-2.ec2.cloud.redislabs.com",
   db: 0,
   // password: process.env.REDIS_AUTH
-  password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+  password: "mkZgFfZX71wrayQne37XbCUdgAfC0a46",
 };
+
+// const conn = {
+//   port: 6379,
+//   host: "127.0.0.1",
+//   db: 0,
+//   // password: process.env.REDIS_AUTH
+//   password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+// };
 
 const redisDb = new redis(conn);
 module.exports = redisDb;
