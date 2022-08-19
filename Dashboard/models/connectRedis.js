42b9761466bd426c798548bfb2a9850c31bd4ba2
AlmogJakov <almog1006@gmail.com>
2022-08-19 14:34:31 +0300

    changed Dashboard design

--- Dashboard/models/connectRedis.js
+++ Dashboard/models/connectRedis.js
@@ -1,6 +1,8 @@
 const redis = require("ioredis");
 
 // https://app.redislabs.com/#/databases/11176232/subscription/1827195/view-bdb/metric
+
+// Connect Using Redis Cloud Details
 const conn = {
   port: 12938,
   host: "redis-12938.c59.eu-west-1-2.ec2.cloud.redislabs.com",
@@ -9,6 +11,7 @@ const conn = {
   password: "mkZgFfZX71wrayQne37XbCUdgAfC0a46",
 };
 
+// Connect Using Local Redis Details (Via Docker)
 // const conn = {
 //   port: 6379,
 //   host: "127.0.0.1",
